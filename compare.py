8d33c747898a4edf9f699e8fedee91295200a33b
Teodor Romanus <trom@ESTATE.local>
2019-01-21 20:42:07 +0200

    add __main__

--- compare.py
+++ compare.py
@@ -11,24 +11,26 @@ import bm3d_impl
 import nlm_impl
 import lpg_pca_impl
 
-noise_variance = 40.0 / 255.0
+if __name__ == '__main__':
 
-algorithms = [mf_impl, bm3d_impl, nlm_impl, lpg_pca_impl]
-psnrs = np.zeros(len(algorithms))
+    noise_variance = 40.0 / 255.0
 
-img_names = os.listdir(os.path.join(os.path.dirname(__file__), 'images'))
-print()
-print("                     [mf          bm3d        nlm        lpg-pca      ]")
-for img_name in img_names:
-    pure_image = readImg(os.path.join('images', img_name))
+    algorithms = [mf_impl, bm3d_impl, nlm_impl, lpg_pca_impl]
+    psnrs = np.zeros(len(algorithms))
 
-    noised_image = getNoisedImage(pure_image, noise_variance)
+    img_names = os.listdir(os.path.join(os.path.dirname(__file__), 'images'))
+    print()
+    print("                     [mf          bm3d        nlm        lpg-pca      ]")
+    for img_name in img_names:
+        pure_image = readImg(os.path.join('images', img_name))
 
-    for i, algorithm in enumerate(algorithms):
-        denoised_image = algorithm.denoise(noised_image, noise_variance)
-        psnr = compare_psnr(pure_image, denoised_image)
-        psnrs[i] = psnr
+        noised_image = getNoisedImage(pure_image, noise_variance)
 
-    print(img_name.ljust(20), psnrs)
+        for i, algorithm in enumerate(algorithms):
+            denoised_image = algorithm.denoise(noised_image, noise_variance)
+            psnr = compare_psnr(pure_image, denoised_image)
+            psnrs[i] = psnr
 
-print()
+        print(img_name.ljust(20), psnrs)
+
+    print()
